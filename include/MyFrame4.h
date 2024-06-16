6ba172a47566e9f1b961c7414a08faf3badc58f1
cqfajia@outlook.com <cqfajia@outlook.com>
2024-06-16 22:30:15 +0800

    init

---
+++ include/MyFrame4.h
@@ -0,0 +1,108 @@
+﻿///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO *NOT* EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <wx/artprov.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/intl.h>
+#include <wx/datectrl.h>
+#include <wx/dateevt.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/string.h>
+#include <wx/timectrl.h>
+#include <wx/calctrl.h>
+#include <wx/scrolbar.h>
+#include <wx/sizer.h>
+#include <wx/panel.h>
+#include <wx/bitmap.h>
+#include <wx/image.h>
+#include <wx/icon.h>
+#include <wx/dataview.h>
+#include <wx/stattext.h>
+#include <wx/textctrl.h>
+#include <wx/combobox.h>
+#include <wx/bmpcbox.h>
+#include <wx/choice.h>
+#include <wx/listbox.h>
+#include <wx/checkbox.h>
+#include <wx/radiobox.h>
+#include <wx/radiobut.h>
+#include <wx/statline.h>
+#include <wx/slider.h>
+#include <wx/gauge.h>
+#include <wx/srchctrl.h>
+#include <wx/clrpicker.h>
+#include <wx/listbook.h>
+#include <wx/listctrl.h>
+#include <wx/scrolwin.h>
+#include <wx/aui/auibook.h>
+#include <wx/menu.h>
+#include <wx/frame.h>
+
+///////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class MyFrame4
+///////////////////////////////////////////////////////////////////////////////
+class MyFrame4 : public wxFrame
+{
+private:
+
+protected:
+    wxListbook* m_listbook14;
+    wxPanel* m_panel34;
+    wxDatePickerCtrl* m_datePicker1;
+    wxTimePickerCtrl* m_timePicker1;
+    wxCalendarCtrl* m_calendar1;
+    wxScrollBar* m_scrollBar1;
+    wxPanel* m_panel35;
+    wxDataViewListCtrl* m_dataViewListCtrl4;
+    wxPanel* m_panel36;
+    wxStaticText* m_staticText3;
+    wxTextCtrl* m_textCtrl9;
+    wxStaticText* m_staticText4;
+    wxTextCtrl* m_textCtrl10;
+    wxComboBox* m_comboBox1;
+    wxBitmapComboBox* m_bcomboBox1;
+    wxChoice* m_choice1;
+    wxListBox* m_listBox2;
+    wxCheckBox* m_checkBox1;
+    wxCheckBox* m_checkBox2;
+    wxCheckBox* m_checkBox3;
+    wxCheckBox* m_checkBox4;
+    wxRadioBox* m_radioBox1;
+    wxRadioButton* m_radioBtn1;
+    wxStaticLine* m_staticline1;
+    wxSlider* m_slider5;
+    wxGauge* m_gauge5;
+    wxPanel* m_panel37;
+    wxSearchCtrl* m_searchCtrl1;
+    wxColourPickerCtrl* m_colourPicker1;
+    wxTimePickerCtrl* m_timePicker2;
+    wxDatePickerCtrl* m_datePicker2;
+    wxAuiNotebook* m_auinotebook4;
+    wxScrolledWindow* m_scrolledWindow4;
+    wxScrolledWindow* m_scrolledWindow5;
+    wxScrolledWindow* m_scrolledWindow6;
+    wxScrolledWindow* m_scrolledWindow7;
+    wxMenuBar* m_menubar6;
+    wxMenu* m_menu10;
+    wxMenu* m_menu11;
+    wxMenu* m_menu12;
+
+public:
+
+    MyFrame4( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("数采工具"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 800,650 ), long style = wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL );
+
+    ~MyFrame4();
+
+};
+
